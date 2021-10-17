d14eed476e1b34d58910fad20bfe38243393dbe1
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-10-17 16:12:35 +0900

    Add default suggestion and fix tab control.

--- background.js
+++ background.js
@@ -3,7 +3,14 @@ const domains = [
   'docs.ruby-lang.org/ja',
   'github.com',
   'developer.mozilla.org',
+  'developer.chrome.com/docs/extensions/'
 ];
+
+chrome.omnibox.onInputStarted.addListener(() => {
+  chrome.omnibox.setDefaultSuggestion({
+    description: 'Select domain below.'
+  });
+})
 
 chrome.omnibox.onInputChanged.addListener((text, suggest) => {
   const suggestions = domains.map(domain => { return { content: domain + ' ' + text, description: domain } })
@@ -12,5 +19,5 @@ chrome.omnibox.onInputChanged.addListener((text, suggest) => {
 
 chrome.omnibox.onInputEntered.addListener((text) => {
   const newURL = 'https://www.google.com/search?q=' + encodeURIComponent(text);
-  chrome.tabs.create({ url: newURL });
+  chrome.tabs.update({ url: newURL });
 });
