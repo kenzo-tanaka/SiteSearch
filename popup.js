250c8b54914e463a6762fb3b3548c90160a3f37a
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-10-17 19:04:12 +0900

    Add storage local example.

---
+++ popup.js
@@ -0,0 +1,32 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function displayWords() {
+  chrome.storage.local.get(['domains'], function (object) {
+    let pageList = document.getElementById('displayWords');
+    if (object.domains) {
+      searchWords = object.domains
+      for (var i = 0; i < searchWords.length; i++) {
+        let listItem = document.createElement('li');
+        listItem.innerText = searchWords[i]
+        pageList.appendChild(listItem);
+      }
+    }
+  });
+}
+
+displayWords();
+
+document.getElementById('wordSubmit').onclick = function () {
+  let userWords = document.getElementById('userWords').value.trim();
+  chrome.storage.local.get(['domains'], function (object) {
+    let newWords = object.domains || [];
+    newWords.push(userWords);
+    chrome.storage.local.set({ domains: newWords });
+  })
+}
+
+document.getElementById('clearList').onclick = function () {
+  chrome.storage.local.clear();
+}
