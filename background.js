f49c28fffd3f40351a9a62a1554ccc37693b0a44
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-10-17 15:34:47 +0900

    Change manifest version to 2.0 for onInputChanged suggestions

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+  suggest([
+    { content: 'site:railsguide.jp ' + text, description: 'site:railsguide.jp ' }
+  ])
+})
+
 chrome.omnibox.onInputEntered.addListener((text) => {
-  // Encode user input for special characters , / ? : @ & = + $ #
   const target = 'site:railsguides.jp ' + text
   const newURL = 'https://www.google.com/search?q=' + encodeURIComponent(target);
   chrome.tabs.create({ url: newURL });
